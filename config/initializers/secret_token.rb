71cedf7379e2315e98b726ee17309dda6732161a
Gasser Akila <gasser.akila@gmail.com>
2013-01-23 13:40:08 +0200

    Initial commit with setting up the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdayaAdmin::Application.config.secret_token = '459bb88f6e3ffa587a832789913805823574a13cebad076c57b9607957139c2c709d3c585a30574611cdf3dd807fa0964d7c4ff1684dc6a417ad9e22fc1b340a'
