86c65bedfb009878ddb5aebf8e30a1a9acdfd7c9
Hussein <aboelseoud@ubuntu.(none)>
2013-01-27 13:56:17 +0200

    Added primary form for adding a kid into the database. Waiting for log in to check for permissions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 BdayaAdmin::Application.routes.draw do
+  get "kids/list"
+
+  get "kids/new"
+
+  get "kids/edit"
+
+  get "kids/delete"
+
+  get "kids/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,5 +64,5 @@ BdayaAdmin::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
