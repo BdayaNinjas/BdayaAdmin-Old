db5e47c71c1a0ac77ec0b8521c32e0fb00a22d9d
Diab <diab977@gmail.com>
2013-01-29 22:12:22 +0200

    Update issue 26 adding backend Methods

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,9 +2,13 @@ class Request
   include Mongoid::Document
   field :session_type, type: Integer
   field :timing, type: Time
+  field :done , type: Boolean
+  field :room , type: String
 
   has_and_belongs_to_many :needers, class_name: "Member", inverse_of: :needed_requests
   belongs_to :assigned, class_name: "Member", inverse_of: :assigned_requests
 
   belongs_to :session
+
+
 end
