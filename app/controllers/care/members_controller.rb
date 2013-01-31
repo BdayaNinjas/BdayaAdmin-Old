4d002cb2749f77f3b27e1284f5edaadecaf1a369
aboelseoud <hussein.aboelseoud@gmail.com>
2013-01-31 13:05:26 +0200

    Added care/members namespace

---
+++ app/controllers/care/members_controller.rb
@@ -0,0 +1,39 @@
+class Care::MembersController < Care::CareController
+	
+  def index
+    @members = Member.all.where(:committee => 'Care')
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+   
+  def edit
+    @member = Member.find(params[:id])
+  end
+   
+  def update
+    @member = Member.find(params[:id])
+    # Update the object
+    if @member.update_attributes(params[:member])
+      # If update succeeds, redirect to the show action
+      flash[:notice] = "Member updated."
+      redirect_to(:action => 'show', :id => @member.id)
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('edit')
+    end
+  end
+   
+  def delete
+    @member = Member.find(params[:id])
+  end
+   
+  def destroy
+    member = Member.find(params[:id])
+    member.destroy
+    flash[:notice] = "Member deleted."
+    redirect_to(:action => 'index')
+  end
+
+end
---
+++ app/helpers/care/members_helper.rb
@@ -0,0 +1,2 @@
+module Care::MembersHelper
+end
