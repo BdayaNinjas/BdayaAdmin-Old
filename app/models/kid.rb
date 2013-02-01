bb5c49cd31ce2de8bafcf9697b9751f7981d23a2
aboelseoud <hussein.aboelseoud@gmail.com>
2013-02-01 11:54:58 +0200

    Removed duplicate flash and added some validations to Kid form.

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -3,9 +3,13 @@ class Kid
   include Mongoid::Timestamps
 
   field :name, type: String
+  validates_presence_of :name, :message => "Name must be provided."
   field :age, type: Integer
+  validates :age, :presence => true
+  validates_numericality_of :age, :greater_than => 0, :less_than => 19, :message => "Age has to be a number between 1 and 18."
   field :address, type: String
   field :strengths, type: String
   field :weaknesses, type: String
   field :notes, type: String
+
 end
