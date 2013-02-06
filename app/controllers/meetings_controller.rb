9187bd787d29fe3bb9b1d8342ff56e6a18b9036d
Hazem <Hazem.kilisly@gmail.com>
2013-02-06 19:09:10 +0200

    meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -34,10 +34,11 @@ class MeetingsController < ApplicationController
   # PUT /meetings/1.json
   def update
     @meeting = Meeting.find(params[:id])
-    @meeting.update_attributes(:title => params[:meeting][:title],:meeting_minutes1 => params[:meeting][:meeting_minutes1], :meeting_minutes2 => params[:meeting][:meeting_minutes2], :meeting_minutes3 => params[:meeting][:meeting_minutes3], :room => params[:meeting][:room])
+    @meeting.update_attributes(params[:meeting])
     @meeting.attendees.each do |attender|
-      if(params[:meeting]['attendance_#{attender.id}'] != nil)
-        @meeting.attendance << [attender.id,params[:meeting]['attendance_#{attender.id}']]
+      param_att = "attendance_"+attender.id
+      if(params[param_att])
+        @meeting.attendance << [attender.id,params[param_att]]
       end
     end
     @meeting.save
