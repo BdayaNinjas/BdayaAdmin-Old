ba9e2fdd58af26951c19fb976a49b2264b121163
aboelseoud <hussein.aboelseoud@gmail.com>
2013-02-06 18:11:57 +0200

    Removed duplicate line in calendar.rb

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,10 +1,9 @@
 class Committee
 	include Mongoid::Document
-	include Mongoid::Timestamps
 
   	field :name, type:String
   	filed :type, type:Integer
 
- has_one :head, class_name: "Member", inverse_of: :hcommittee
- has_many :vices, class_name: "Member", inverse_of: :vcommittee
- has_many :members, class_name: "Member", inverse_of: :committee
+	has_one :head, class_name: "Member", inverse_of: :hcommittee
+	has_many :vices, class_name: "Member", inverse_of: :vcommittee
+	has_many :members, class_name: "Member", inverse_of: :committee
