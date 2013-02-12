d0f271ca64ca4e9f5e284c18a68753b7ef61809a
aboelseoud <hussein.aboelseoud@gmail.com>
2013-02-12 20:30:28 +0200

    Added editing functionality to Care index.

--- app/controllers/care/members_controller.rb
+++ app/controllers/care/members_controller.rb
@@ -10,12 +10,7 @@ class Care::MembersController < Care::CareController
     @member = Member.find(params[:id])
     # Update the object
     if @member.update_attributes(params[:member])
-      # If update succeeds, redirect to the show action
       flash[:notice] = "Member updated."
-      redirect_to(:action => 'show', :id => @member.id)
-    else
-      # If save fails, redisplay the form so user can fix problems
-      render('edit')
     end
   end
    
@@ -23,7 +18,6 @@ class Care::MembersController < Care::CareController
     member = Member.find(params[:id])
     member.destroy
     flash[:notice] = "Member deleted."
-    redirect_to(:action => 'index')
   end
 
 end
--- app/controllers/care_controller.rb
+++ app/controllers/care_controller.rb
@@ -3,5 +3,21 @@ class CareController < ApplicationController
 		@members = Committee.find_by(:name => "Care").members.where(:role => 'Member')
     	@head = Committee.find_by(:name => "Care").members.where(:role => 'Head')[0]
     	@vices = Committee.find_by(:name => "Care").vices.where(:role => 'Vice Head')
+    	@kids = Kid.all.order_by([[ :age, :asc ]])
 	end
+
+	def updateMember
+        @member = Member.find(params[:id])
+        # Update the object
+        if @member.update_attributes(params[:member])
+           flash[:notice] = "Member updated."
+        end
+    end
+   
+    def destroyMember
+        member = Member.find(params[:id])
+        member.destroy
+        flash[:notice] = "Member deleted."
+    end
+
 end
