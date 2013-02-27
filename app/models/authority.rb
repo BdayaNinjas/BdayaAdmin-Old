a4b506da80209d7bf9959093fa0374e0f9db97ff
Hazem <Hazem.kilisly@gmail.com>
2013-02-28 00:20:45 +0200

    authorities model with documentation

---
+++ app/models/authority.rb
@@ -0,0 +1,48 @@
+# Author: Hazem Tawfik
+class Authority
+  include Mongoid::Document
+
+  field :name, type:String
+  field :tubles, type:Array
+
+# We should already have authorities (for each action on the website) created like Committies first on launching the website in the Seeds file
+	
+	# On the authorities page in the admin view he can show every action on the website and the corresbonding authorities in multiple drop-down lists that can be added as more than one
+	
+	# It'll be saved when updating Authorities page in Admin, in the form (example):
+		# auth = Authority.find_by(name: "update_yes")
+		# auth.tubles = Params[tubles] // if updating the whole authorities of that Authority
+			# or// auth.tubles << Params[tubles] // when just adding new authority
+		# auth.save
+
+	# The Authority will be in the form:
+		# name => "edit_yes"
+		# tubles => [[yes(a Committee Object), "head"],[geneh, "vises"], [geneh, "members"]]
+	
+	# Easily this way,, You can just check the authorization of a user in the form:
+		# Authority.has_authority(Member.last, "create_geneh_case")
+
+  def has_authority(user, action)
+  	auth = Authority.find_by(name: action)
+  	if(auth != nil)
+  		flag = false
+  		auth.tubles.each do |ath|
+  			if(ath[1] == "head")
+  				if(ath[0].head == user)
+  					flag = true
+  				end
+  			elsif(ath[1] == "vises")
+  				if(ath[0].vices.include?(user))
+  					flag = true
+  				end
+  			elsif(ath[1] == "members")
+  				if(ath[0].members.include?(user))
+  					flag = true
+  				end
+  			end
+  		end
+  		return flag
+  	end
+  end
+
+end
